456a92864f51eb38f9c25cda90f757e0b578f440
User <ulyanovskiy.2001@mail.ru>
2026-02-06 17:13:09 +0300

    feat: first fast endpoints usage

--- Product.Api/Endpoints/Commands/CreateProductEndpoint.cs
+++ Product.Api/Endpoints/Commands/CreateProductEndpoint.cs
@@ -1,5 +1,19 @@
-﻿namespace ProductApi.Endpoints.Commands;
+﻿using FastEndpoints;
+using ProductApi.DTOs.Requests;
+using ProductApi.DTOs.Responses;
 
-public class CreateProductEndpoint
+namespace ProductApi.Endpoints.Commands;
+
+public class CreateProductEndpoint : Endpoint<CreateProductRequest, ProductResponse>
 {
+    public override void Configure()
+    {
+        Post("api/product/create");
+        AllowAnonymous();
+    }
+
+    public override Task<ProductResponse> ExecuteAsync(CreateProductRequest req, CancellationToken ct)
+    {
+        return base.ExecuteAsync(req, ct);
+    }
 }
